7e7c426bd66af33c1a37c627d29532075e33e794
KhalidFinny <katthoriq10@gmail.com>
2025-09-03 11:57:05 +0700

    #03 Pengantar bahasa pemograman dart - bagian 2

---
+++ 03/tugasPraktikum_03.dart
@@ -0,0 +1,18 @@
+void main() {
+  String nama = "Muhammad Khalid atthoriq";
+  String nim = "2341720114";
+
+  for (int i = 0; i <= 201; i++) {
+    if (isPrima(i)) {
+      print("Bilangan prima: $i | Nama: $nama | NIM: $nim");
+    }
+  }
+}
+
+bool isPrima(int angka) {
+  if (angka < 2) return false;
+  for (int i = 2; i * i <= angka; i++) {
+    if (angka % i == 0) return false;
+  }
+  return true;
+}
