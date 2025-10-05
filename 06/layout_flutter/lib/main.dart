6ed1d392db19b3cef459298f776f0acf316e15db
KhalidFinny <katthoriq10@gmail.com>
2025-10-05 10:13:36 +0700

    Praktikum 2

--- 06/layout_flutter/lib/main.dart
+++ 06/layout_flutter/lib/main.dart
@@ -8,15 +8,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget titleSection = Container(
-      padding: const EdgeInsets.all(32), 
+      padding: const EdgeInsets.all(32),
       child: Row(
         children: [
           Expanded(
-            /* soal 1 */
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start, 
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                /* soal 2 */
                 Container(
                   padding: const EdgeInsets.only(bottom: 8),
                   child: const Text(
@@ -35,14 +33,24 @@ class MyApp extends StatelessWidget {
               ],
             ),
           ),
-          /* soal 3 */
           const Icon(
             Icons.star,
-            color: Colors.red, 
+            color: Colors.red,
           ),
           const Text('41'),
         ],
       ),
+    );
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
     );
 
     return MaterialApp(
@@ -51,8 +59,34 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Flutter layout demo'),
         ),
-        body: titleSection, // ganti "Hello World" dengan titleSection
+        body: Column(
+          children: [
+            titleSection,
+            buttonSection,
+          ],
+        ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
