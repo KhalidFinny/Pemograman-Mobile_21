a5e7bf28f288f4416298ab5fd944328910b2dda2
KhalidFinny <katthoriq10@gmail.com>
2025-10-07 17:02:27 +0700

    tugas praktikum

--- 06/belanja/README.md
+++ 06/belanja/README.md
@@ -1,16 +1,6 @@
 # navigation_flutter
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+![screenshot](images/08.png)
+![screenshot](images/09.png)
+![screenshot](images/11.png)
+![screenshot](images/12.png)
--- 06/belanja/lib/pages/home_page.dart
+++ 06/belanja/lib/pages/home_page.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
 import '../models/item.dart';
+import '../widgets/product_card.dart';
 
 class HomePage extends StatelessWidget {
   final List<Item> items = [
-    Item(name: 'Sugar', price: 5000),
-    Item(name: 'Salt', price: 2000),
+    Item(
+      name: 'Sugar Premium', 
+      price: 15000,
+      imageUrl: 'assets/fire.png',
+      stock: 50,
+      rating: 4.5,
+    ),
+    Item(
+      name: 'Salt Himalaya', 
+      price: 8000,
+      imageUrl: 'assets/fire.png',
+      stock: 30,
+      rating: 4.2,
+    ),
+    Item(
+      name: 'Rice Premium', 
+      price: 25000,
+      imageUrl: 'assets/fire.png',
+      stock: 25,
+      rating: 4.8,
+    ),
+    Item(
+      name: 'Oil Cooking', 
+      price: 18000,
+      imageUrl: 'assets/fire.png',
+      stock: 40,
+      rating: 4.3,
+    ),
+    Item(
+      name: 'Flour Wheat', 
+      price: 12000,
+      imageUrl: 'assets/fire.png',
+      stock: 35,
+      rating: 4.1,
+    ),
+    Item(
+      name: 'Milk Fresh', 
+      price: 22000,
+      imageUrl: 'assets/fire.png',
+      stock: 20,
+      rating: 4.6,
+    ),
   ];
 
   HomePage({super.key});
@@ -12,36 +53,62 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Item List')),
+      appBar: AppBar(
+        title: const Text('Marketplace Belanja'),
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
+        elevation: 0,
+      ),
       body: Container(
-        margin: EdgeInsets.all(8),
-        child: ListView.builder(
-          padding: EdgeInsets.all(8),
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            final item = items[index];
-            return InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, '/item');
-              },
-              child: Card(
-                child: Container(
-                  margin: EdgeInsets.all(8),
-                  child: Row(
-                    children: [
-                      Expanded(child: Text(item.name)),
-                      Expanded(
-                        child: Text(
-                          item.price.toString(),
-                          textAlign: TextAlign.end,
-                        ),
-                      ),
-                    ],
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.deepPurple,
+              Colors.white,
+            ],
+            stops: [0.1, 0.1],
+          ),
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                    childAspectRatio: 0.75,
                   ),
+                  itemCount: items.length,
+                  itemBuilder: (context, index) {
+                    return ProductCard(item: items[index]);
+                  },
                 ),
               ),
-            );
-          },
+            ),
+            Container(
+              padding: const EdgeInsets.all(16),
+              color: Colors.grey[100],
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.shopping_cart, color: Colors.deepPurple),
+                  SizedBox(width: 8),
+                  Text(
+                    'Muhammad Khalid Atthoriq - 2341720114',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
